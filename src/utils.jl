82f3597211fb6c3e74b1adbda62a658ef8575eb5
Chad Scherrer <chad.scherrer@gmail.com>
2019-07-20 06:25:21 -0700

    comment out body of @check_args

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,14 @@
 ## macro for argument checking
 
 macro check_args(D, cond)
-    quote
-        if !($(esc(cond)))
-            throw(ArgumentError(string(
-                $(string(D)), ": the condition ", $(string(cond)), " is not satisfied.")))
-        end
-    end
+    quote end
+    
+    # quote
+    #     if !($(esc(cond)))
+    #         throw(ArgumentError(string(
+    #             $(string(D)), ": the condition ", $(string(cond)), " is not satisfied.")))
+    #     end
+    # end
 end
 
 ## a type to indicate zero vector
